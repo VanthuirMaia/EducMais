61d3f229e8a37be1348e6a6d1c907ea168e94d64
Bowie <vanmaiasf@gmail.com>
2024-10-01 00:24:36 -0300

    Criadas Class do Models

--- PlanLab/models.py
+++ PlanLab/models.py
@@ -4,3 +4,15 @@ class Usuario(models.Model):
     email = models.EmailField(unique=True)
     senha = models.models.CharField(max_length=100)
 
+class Aula(models.Model):
+    data_aula = models.DateField()
+    turma = models.CharField(max_length=100)
+    semestre = models.CharField(max_length=10)
+    titulo = models.CharField(max_length=200)
+    conteudo_programatico = models.TextField()
+    metodologia = models.TextField()
+    recursos_necessarios = models.TextField()
+    avaliacao_observacoes = models.TextField()
+    observacoes = models.TextField()
+    eventos_extraordinarios = models.TextField()
+    uuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
