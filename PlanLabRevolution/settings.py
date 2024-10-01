fbbd994a1023defeb3e917d23c3ef8496da7b871
Bowie <vanmaiasf@gmail.com>
2024-10-01 00:20:13 -0300

    Realizadas Migrações, criado DB Postgres, iniciando os Models

--- PlanLabRevolution/settings.py
+++ PlanLabRevolution/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'PlanLabRevolution.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'planlab_db',
+        'USER': 'planlab_user',
+        'PASSWORD': 'acdvPL2846#',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
