122e1d8e1e29fedc267a4cd9437fb034d67edec4
Bowie <vanmaiasf@gmail.com>
2024-11-29 20:34:10 -0300

    final

--- PlanLab/models.py
+++ PlanLab/models.py
@@ -1,81 +1,85 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
+# Modelo para representar uma disciplina
 class Disciplina(models.Model):
     nome = models.CharField(max_length=100)
 
     def __str__(self):
         return self.nome
 
+
+# Modelo para representar uma turma
 class Turma(models.Model):
     nome = models.CharField(max_length=100)
 
     def __str__(self):
         return self.nome
 
+
+# Modelo para representar um semestre
 class Semestre(models.Model):
     descricao = models.CharField(max_length=50)
 
     def __str__(self):
         return self.descricao
 
+
+# Modelo para representar uma aula
 class Aula(models.Model):
-    # Campos do modelo Aula
+    # Relacionamentos
     disciplina = models.ForeignKey(Disciplina, on_delete=models.CASCADE)
-    data_aula = models.DateField(verbose_name='Data da Aula')
     turma = models.ForeignKey(Turma, on_delete=models.CASCADE)
     semestre = models.ForeignKey(Semestre, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Usuário')
+    
+    # Campos principais
+    data_aula = models.DateField(verbose_name='Data da Aula')
     titulo = models.CharField(max_length=200, verbose_name='Título')
     eventos_extraordinarios = models.CharField(
-        max_length=255, 
-        blank=True, 
-        null=True, 
-        verbose_name='Eventos Extraordinários'
+        max_length=255, blank=True, null=True, verbose_name='Eventos Extraordinários'
     )
     conteudo_programatico = models.TextField(verbose_name='Conteúdo Programático')
     metodologia = models.TextField(verbose_name='Metodologia')
     recursos_necessarios = models.TextField(verbose_name='Recursos Necessários')
     avaliacao_observacoes = models.TextField(
-        blank=True, 
-        null=True, 
-        verbose_name='Avaliação e Observações'
+        blank=True, null=True, verbose_name='Avaliação e Observações'
     )
     observacoes = models.TextField(blank=True, null=True, verbose_name='Observações')
     
-    # Chave estrangeira para o usuário
-    usuario = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Usuário')
-
+    # Configurações do modelo
     class Meta:
         verbose_name = 'Aula'
         verbose_name_plural = 'Aulas'
         ordering = ['data_aula']  # Ordenar as aulas pela data da aula
 
+    def __str__(self):
+        return f'{self.titulo} - {self.disciplina}'
+
+
+# Modelo para representar uma caderneta de aula
 class Caderneta(models.Model):
-    # Campos do modelo Caderneta
-    data_aula = models.DateField(verbose_name='Data da Aula')
+    # Relacionamentos
     disciplina = models.ForeignKey(Disciplina, on_delete=models.CASCADE)
     turma = models.ForeignKey(Turma, on_delete=models.CASCADE)
     semestre = models.ForeignKey(Semestre, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Usuário')
+
+    # Campos principais
+    data_aula = models.DateField(verbose_name='Data da Aula')
     titulo = models.CharField(max_length=200, verbose_name='Título')
     eventos = models.CharField(
-        max_length=255, 
-        blank=True, 
-        null=True, 
-        verbose_name='Eventos'
+        max_length=255, blank=True, null=True, verbose_name='Eventos'
     )
     conteudo = models.TextField(verbose_name='Conteúdo')
     materiais = models.TextField(blank=True, null=True, verbose_name='Materiais')
     atividade = models.TextField(blank=True, null=True, verbose_name='Atividade')
 
-    usuario = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Usuário')
-
-    def __str__(self):
-        return f'{self.titulo} - {self.disciplina}'
-
+    # Configurações do modelo
     class Meta:
         verbose_name = 'Caderneta'
         verbose_name_plural = 'Cadernetas'
         ordering = ['data_aula']  # Ordenar as cadernetas pela data da aula
 
-
+    def __str__(self):
+        return f'{self.titulo} - {self.disciplina}'
